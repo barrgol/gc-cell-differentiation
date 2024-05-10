4726b6e70d2bfd5090f564d3c2e8db8bc34521c3
wykremcony <bartek.golik@student.uva.nl>
2024-05-10 02:31:29 +0200

    Final commit before submission

--- CD40Subnetwork.py
+++ CD40Subnetwork.py
@@ -1,9 +1,44 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
-from scipy.stats import norm
 
-def model(r0, t, k_r, sigma_r, mu_r, lambda_r, cdt):
+default_params = {
+    "mu_r" : 0.1,
+    "sigma_r" : 2.6,
+    "k_r" : 1,
+    "lambda_r" : 1
+}
+
+def model(r0, t, params):
+    """Returns the ODE for CD40 subnetwork.
+
+    Since the model includes many parameters, they were collapsed into a single dictionary argument 'params'.
+
+    Args:
+        r0 (float): initial IRF4 value
+        t (np.array[float]): time steps
+        params (dict): dictionary with parameters
+    
+    'params' dictionary contains the following entries:
+        k_r: dissociation constant
+        sigma_r: maximum transcription rate
+        mu_r: basal production rate
+        lambda_r: degradation rate
+        cdt: time-dependent functions for calculating cd0(t)
+    """
+
+    # Checking validity of input parameters
+    acceptable_names = [ "k_r", "sigma_r", "mu_r", "lambda_r", "cdt" ]
+    
+    for param in params.keys():
+        assert param in acceptable_names
+
+    # UNpack params dictionary
+    k_r = params["k_r"]
+    sigma_r = params["sigma_r"]
+    mu_r = params["mu_r"]
+    lambda_r = params["lambda_r"]
+    cdt = params["cdt"]
 
     # BCL6 is downregulated and thus repression is abrogated
     CD40 = cdt(t)
@@ -11,12 +46,31 @@ def model(r0, t, k_r, sigma_r, mu_r, lambda_r, cdt):
 
     return drdt
 
-def solve_model(r0, t, k_r, sigma_r, mu_r, lambda_r, cdt): 
-    return odeint(model, r0, t, args=(k_r, sigma_r, mu_r, lambda_r, cdt))
+def solve_model(r0, t, params):
+    """Solves the model in time given the set of parameters and inital values.
+
+    Args:
+        r0 (float): initial IRF4 value
+        t (np.array[float]): time steps
+        params (dict): dictionary with parameters
+
+    Returns:
+        np.array[np.array[float]]: solutions to the model
+    """  
+    return odeint(model, r0, t, args=(params,))
 
 def plot_model(ax, t, sol):
+    """Plots the model solution.
+
+    Args:
+        ax (matplotlib.axes.Axes): Matplotlib's ax to plot the model to
+        t (np.array[float]): time steps
+        sol (np.array[np.array[float]]): solution returned by solve_model
+    """
+    # Plot model solution over time
     ax.plot(t, sol[:, 0], 'blue', label='IRF4', lw=2.0)
     
+    # Make the plot pretty
     ax.set_xlabel('t')
     ax.set_ylabel(r"Expression level $[C_{0}=10^{-8}]$M")
     ax.set_title("Model evolution in time")
@@ -26,7 +80,17 @@ def plot_model(ax, t, sol):
     ax.legend(loc='best')
     ax.grid()
 
-def plot_signals(ax, t, cdt):
+def plot_signals(ax, t, params):
+    """Plots the CD40 signal over time, corresponding to model solution (as there
+    is no repression term, the signal is only depending on crt parameter).
+
+    Args:
+        ax (matplotlib.axes.Axes): Matplotlib's ax to plot the model to
+        t (np.array[float]): time steps
+        params (dict): dictionary with parameters for which the solutions were computed
+    """
+
+    cdt = params["cdt"]
     CD40 = cdt(t)
     ax.plot(t, CD40, 'blue', label='CD40')
     
--- GCExitPathwayModel.py
+++ GCExitPathwayModel.py
@@ -3,33 +3,74 @@ import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 from scipy.stats import norm
 
-def model(y0, t, k, sigma, mu, lam, bcrt, cdt):
+default_params = {
+    "mu_p" : 1e-6,
+    "mu_b" : 2,
+    "mu_r" : 0.1,
+
+    "sigma_p" : 9,
+    "sigma_b" : 100,
+    "sigma_r" : 2.6,
+
+    "k_p" : 1,
+    "k_b" : 1,
+    "k_r" : 1,
+
+    "lambda_p" : 1,
+    "lambda_b" : 1,
+    "lambda_r" : 1
+}
+
+def model(y0, t, params):
     """Returns the system of ODEs corresponding to change of
     transcriptional activity in time [dpdt, dbdt, drdt].
+
+    Since the model includes many parameters, they were collapsed into a single dictionary argument 'params'.
 
     Args:
         y0 (np.array[float]): initial factor values of p (BLIMP1), b (BCL6) and r (IRF4)
         t (np.array[float]): time steps
-        k (np.array[float]): dissociation constants 
-        sigma (np.array[float]): maximum transcription rates
-        mu (np.array[float]): basal production rates
-        lam (np.array[float]): degradation rates
-        bcrt ((float) -> (float)): time-dependent function for calculating bcr0(t)
-        cdt ((float) -> (float)): time-dependent function for calculating cd0(t)
+        params (dict): dictionary with parameters
+    
+    'params' dictionary contains the following entries:
+        k_p, k_b, k_r: dissociation constants 
+        sigma_p, sigma_b, sigma_r: maximum transcription rates
+        mu_p, mu_b, mu_r: basal production rates
+        lambda_p, lambda_b, lambda_r: degradation rates
+        bcrt, cdt: time-dependent functions for calculating bcr0(t) and cd0(t)
     """
 
-    p, b, r = y0
-    kp, kb, kr = k
-    sp, sb, sr = sigma
-    mp, mb, mr = mu
-    lp, lb, lr = lam
+    # Checking validity of input parameters
+    acceptable_names = [
+            "k_p", "k_b", "k_r",
+            "sigma_p", "sigma_b", "sigma_r",
+            "mu_p", "mu_b", "mu_r",
+            "lambda_p", "lambda_b", "lambda_r",
+            "bcrt", "cdt"
+        ]
+    
+    for param in params.keys():
+        assert param in acceptable_names
 
+    # Initial values
+    p, b, r = y0
+
+    # UNpack params dictionary
+    kp, kb, kr = params["k_p"], params["k_b"], params["k_r"]
+    sp, sb, sr = params["sigma_p"], params["sigma_b"], params["sigma_r"]
+    mp, mb, mr = params["mu_p"], params["mu_b"], params["mu_r"]
+    lp, lb, lr = params["lambda_p"], params["lambda_b"], params["lambda_r"]
+    bcrt, cdt = params["bcrt"], params["cdt"]
+
+    # Calculate unrepressed BCR and CD40 signal
     bcr0 = bcrt(t)
     cd0 = cdt(t)
 
+    # Calculate repressed BCR and CD40 signal
     BCR = bcr0 * ((kb**2) / (kb**2 + b**2))
     CD40 = cd0 * ((kb**2) / (kb**2 + b**2))
 
+    # ODE system
     dpdt = mp + sp * ((kb**2) / (kb**2 + b**2)) + sp * ((r**2) / (kr**2 + r**2)) - (lp * p)
     dbdt = mb + sb * ((kp**2) / (kp**2 + p**2)) * ((kb**2) / (kb**2 + b**2)) * ((kr**2) / (kr**2 + r**2)) - (lb + BCR) * b
     drdt = mr + sr * ((r**2) / (kr**2 + r**2)) + CD40 - (lr * r)
@@ -37,23 +78,18 @@ def model(y0, t, k, sigma, mu, lam, bcrt, cdt):
 
     return dydt
 
-def solve_model(y0, t, k, sigma, mu, lam, bcrt, cdt):
+def solve_model(y0, t, params):
     """Solves the model in time given the set of parameters and inital values.
 
     Args:
-        y0 (np.array[float]): initial values
+        y0 (np.array[float]): initial factor values of p (BLIMP1), b (BCL6) and r (IRF4)
         t (np.array[float]): time steps
-        k (np.array[float]): dissociation constants 
-        sigma (np.array[float]): maximum transcription rates
-        mu (np.array[float]): basal production rates
-        lam (np.array[float]): degradation rates
-        bcrt ((float) -> (float)): time-dependent function for calculating bcr0(t)
-        cdt ((float) -> (float)): time-dependent function for calculating cd0(t)
+        params (dict): dictionary with parameters
 
     Returns:
         np.array[np.array[float]]: solutions to the model
     """    
-    return odeint(model, y0, t, args=(k, sigma, mu, lam, bcrt, cdt))
+    return odeint(model, y0, t, args=(params,))
 
 def plot_model(ax, t, sol):
     """Plots the model solutions.
@@ -62,11 +98,13 @@ def plot_model(ax, t, sol):
         ax (matplotlib.axes.Axes): Matplotlib's ax to plot the model to
         t (np.array[float]): time steps
         sol (np.array[np.array[float]]): solutions returned by solve_model
-    """    
+    """
+    # Plot model solutions over time
     ax.plot(t, sol[:, 0], 'blue', label='BLIMP1', lw=2.0)
     ax.plot(t, sol[:, 1], 'green', label='BCL6', lw=2.0)
     ax.plot(t, sol[:, 2], 'red', label='IRF4', lw=2.0)
     
+    # Make the plot pretty
     ax.set_xlabel('t')
     ax.set_ylabel(r"Expression level $[C_{0}=10^{-8}]$M")
     ax.set_title("Model evolution in time")
@@ -76,24 +114,31 @@ def plot_model(ax, t, sol):
     ax.legend(loc='best')
     ax.grid()
 
-def plot_singals(ax, t, sol, kb, bcrt, cdt):
+def plot_signals(ax, t, params, sol):
     """Plots the CD40 and BCR signals over time, corresponding to model solutions.
 
     Args:
         ax (matplotlib.axes.Axes): Matplotlib's ax to plot the model to
         t (np.array[float]): time steps
-        sol (np.array[np.array[float]]): =solutions returned by scipy.integrate.odeint
-        kb (float): BCL6 dissociation constant used in solution
-        bcrt ((float) -> (float)): time-dependent function for calculating bcr0(t)
-        cdt ((float) -> (float)): time-dependent function for calculating cd0(t)
+        params (dict): dictionary with parameters for which the solutions were computed
+        sol (np.array[np.array[float]]): solutions returned by scipy.integrate.odeint
     """
+    # Unpack the parameters
+    kb = params["k_b"]
+    bcrt, cdt = params["bcrt"], params["cdt"]
+
+    # Calculate input (non-repressed) signals
     bcr0 = bcrt(t)
     cd0 = cdt(t)
 
-    b = sol[:, 1]
+    # Obtain BCL6 to calculate repressed signals
+    b = sol[:,1]
+
+    # Calculate output (repressed) signals
     BCR = bcr0 * ((kb**2) / (kb**2 + b**2))
     CD40 = cd0 * ((kb**2) / (kb**2 + b**2))
 
+    # Plotting
     ax.plot(t, bcr0, 'red', ls="--", label=r'bcr$_0$')
     ax.plot(t, BCR, 'red', label='BCR')
     ax.plot(t, cd0, 'blue', ls="--", label=r'cd$_0$')
@@ -106,19 +151,19 @@ def plot_singals(ax, t, sol, kb, bcrt, cdt):
     ax.legend(loc='best')
     ax.grid()
 
-def bell_curve_signal(t, strength, loc, scale):
+def bell_curve_signal(t, strength, tpeak, scale):
     """Generates a bell curve shaped signal.
 
     Args:
         t (np.array[float]): time steps
         strength (float): signal value at the peak
-        loc (float): location parameter of normal distribution
+        tpeak (float): location parameter of normal distribution
         scale (float): scale parameter of normal distribution
 
     Returns:
         np.array[float]: signal value at the given time steps
     """    
-    return norm.pdf(t, loc=loc, scale=scale) / norm.pdf(loc, loc=loc, scale=scale) * strength
+    return norm.pdf(t, loc=tpeak, scale=scale) / norm.pdf(tpeak, loc=tpeak, scale=scale) * strength
 
 def rectangle_signal(t, strength, tstart, tend):
     """Generates a rectangle shaped signal.
